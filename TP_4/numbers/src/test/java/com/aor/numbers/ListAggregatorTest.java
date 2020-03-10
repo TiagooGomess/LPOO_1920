5d93092a4374817145a18716302c018ea5f91a36
TiagooGomess <tiagogomes2011@live.com.pt>
2020-03-10 15:49:21 +0000

    Added section 3

--- TP_4/numbers/src/test/java/com/aor/numbers/ListAggregatorTest.java
+++ TP_4/numbers/src/test/java/com/aor/numbers/ListAggregatorTest.java
@@ -1,5 +1,6 @@
 package com.aor.numbers;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -8,14 +9,20 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class ListAggregatorTest {
-    @Test
-    public void sum() {
-        List<Integer> list = new ArrayList<>();
+    private List<Integer> list;
+
+    @Before
+    public void helper() {
+        list = new ArrayList<>();
         list.add(1);
         list.add(2);
         list.add(4);
         list.add(2);
         list.add(5);
+    }
+
+    @Test
+    public void sum() {
 
         ListAggregator aggregator = new ListAggregator(list);
 
@@ -26,12 +33,6 @@ public class ListAggregatorTest {
 
     @Test
     public void max() {
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(4);
-        list.add(2);
-        list.add(5);
 
         ListAggregator aggregator = new ListAggregator(list);
 
@@ -42,12 +43,6 @@ public class ListAggregatorTest {
 
     @Test
     public void min() {
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(4);
-        list.add(2);
-        list.add(5);
 
         ListAggregator aggregator = new ListAggregator(list);
 
@@ -58,12 +53,6 @@ public class ListAggregatorTest {
 
     @Test
     public void distinct() {
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(4);
-        list.add(2);
-        list.add(5);
 
         ListAggregator aggregator = new ListAggregator(list);
 
--- TP_4/numbers/src/test/java/com/aor/numbers/ListDeduplicatorTest.java
+++ TP_4/numbers/src/test/java/com/aor/numbers/ListDeduplicatorTest.java
@@ -1,5 +1,6 @@
 package com.aor.numbers;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -9,20 +10,26 @@ import static org.junit.Assert.*;
 
 public class ListDeduplicatorTest {
 
-    @Test
-    public void deduplicate() {
-        List<Integer> list = new ArrayList<>();
+    private List<Integer> list;
+    List<Integer> expected;
+
+    @Before
+    public void helper() {
+        list = new ArrayList<>();
         list.add(1);
         list.add(2);
         list.add(4);
         list.add(2);
         list.add(5);
-
-        List<Integer> expected = new ArrayList<>();
+        expected = new ArrayList<>();
         expected.add(1);
         expected.add(2);
         expected.add(4);
         expected.add(5);
+    }
+
+    @Test
+    public void deduplicate() {
 
         ListDeduplicator deduplicator = new ListDeduplicator(list);
         List<Integer> distinct = deduplicator.deduplicate();
