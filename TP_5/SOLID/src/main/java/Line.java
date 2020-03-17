5439d35390d9ae3cd57c5d0e969e2e3aa26b763c
TiagooGomess <tiagogomes2011@live.com.pt>
2020-03-17 16:34:19 +0000

    Added section 3

---
+++ TP_5/SOLID/src/main/java/Line.java
@@ -0,0 +1,19 @@
+public class Line implements BasicShape {
+    private double length;
+    Line(double length) {
+        this.length = length;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
+}
